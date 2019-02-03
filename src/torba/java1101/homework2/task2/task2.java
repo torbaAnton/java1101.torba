0b5aa773b0b6c6bf9d2a08f2dc79d21f28a47845
torbaAnton <noize.ji@gmail.com>
2019-02-04 00:07:41 +0200

    Check

---
+++ src/torba/java1101/homework2/task2/task2.java
@@ -0,0 +1,7 @@
+package torba.java1101.homework2.task2;
+
+public class task2 {
+    public static void main(String[] args) {
+        System.out.println("tut budet dz");
+    }
+}
---
+++ src/torba/java1101/homework2/task3/task3.java
@@ -0,0 +1,7 @@
+package torba.java1101.homework2.task3;
+
+public class task3 {
+    public static void main(String[] args) {
+        System.out.println("tut budet dz");
+    }
+}
