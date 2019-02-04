8fd3955b5346b14716213618f9a00d704d1b7996
torbaAnton <noize.ji@gmail.com>
2019-02-04 16:02:45 +0200

    Check

--- src/torba/java1101/homework2/task1/task1.java
+++ src/torba/java1101/homework2/task1/task1.java
@@ -1,7 +1,22 @@
 package torba.java1101.homework2.task1;
 
+
+
+//1. Вывести десятичные коды символов своего ФИО
 public class task1 {
     public static void main(String[] args) {
-        System.out.println("tut budet dz");
+        char [] arr2 = {'Т','о','р','б','а'};
+        char [] arr1 = {'А','н','т','о','н'};
+        char [] arr3 = {'А','н','д','р','е','е','в','и','ч'};
+        System.out.println("Торба");
+        for(int i=0;i<arr2.length;i++) {
+            System.out.println((int) arr2[i]);
+        }System.out.println("Антон");
+        for(int i=0;i<arr1.length;i++){
+            System.out.println((int)arr1[i]);
+        }  System.out.println("Андреевич");
+        for(int i=0;i<arr3.length;i++) {
+            System.out.println((int) arr3[i]);
+        }
     }
 }
--- src/torba/java1101/homework2/task2/task2.java
+++ src/torba/java1101/homework2/task2/task2.java
@@ -1,7 +1,60 @@
 package torba.java1101.homework2.task2;
 
 public class task2 {
+   /* 1. Вывести десятичные коды символов своего ФИО
+2. Перенести Фамилию в конец ФИО
+3. Развернуть строку ФИО задом наперед*/
     public static void main(String[] args) {
-        System.out.println("tut budet dz");
+form();
+    }
+    public static void form() {
+        String FIO = "Торба Антон Андреевич";
+        getDecimalLine(FIO);
+        printNewLine();
+        getReversedLine(FIO);
+        printNewLine();
+        getPermutedLine(FIO);
+    }
+
+    private static void getDecimalLine(String FIO) {
+        for (int index = 0; index < FIO.length(); index++) {
+            char character = FIO.charAt(index);
+            if (character == ' ') {
+                System.out.print("space(" + (int) character + ") ");
+            } else {
+                System.out.print((int) character + " ");
+            }
+        }
+    }
+
+    private static void getReversedLine(String FIO) {
+        StringBuffer reversedLine = new StringBuffer(FIO.length());
+        for (int index = FIO.length() - 1; index >= 0; index--) {
+            char ch = FIO.charAt(index);
+            reversedLine.append(ch);
+
+        }
+        System.out.print(reversedLine);
+    }
+
+    private static void getPermutedLine(String FIO) {
+        StringBuffer line = new StringBuffer().append(FIO);
+        int firstSpaceIndex = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ' ') {
+                firstSpaceIndex = i;
+                break;
+            }
+        }
+        String lastName = line.substring(0, firstSpaceIndex);
+        line.delete(0, firstSpaceIndex + 1);
+        line.append(' ').append(lastName);
+
+        System.out.print(line);
+
+    }
+
+    private static void printNewLine() {
+        System.out.println();
     }
 }
